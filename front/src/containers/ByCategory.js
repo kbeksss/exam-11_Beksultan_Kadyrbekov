7e66ade60f7c6682a2f3c0d0720335f633d6396f
kbeksss <kbeksss@outlook.com>
2020-03-21 18:47:08 +0600

    Added filtering on back

--- front/src/containers/ByCategory.js
+++ front/src/containers/ByCategory.js
@@ -1,17 +1,36 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {getByCategory} from "../store/actions/productsActions";
+import ImageThumbnail from "../components/ImageThumbnail/ImageThumbnail";
+import {Badge, Card, Container, Row} from "reactstrap";
+import Sidebar from "../components/Sidebar";
 
 const ByCategory = props => {
     const products = useSelector(state => state.products.products);
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(getByCategory(props.match.params.categoryValue));
-    }, [dispatch, props.match.params.categoryValue]);
+        dispatch(getByCategory(props.match.params.categoryId));
+    }, [dispatch, props.match.params.categoryId]);
     console.log(products);
     return (
         <>
-
+            <Container>
+                <Row>
+                    <Sidebar className='w-25 border-right'/>
+                    <div className='w-75'>
+                        {products.map(product => (
+                            <Card
+                                key={product._id}
+                                className='w-25 d-inline-block m-2 text-center p-1'
+                            >
+                                <ImageThumbnail image={product.image}/>
+                                <p>{product.title}</p>
+                                <Badge>{product.price}</Badge>
+                            </Card>
+                        ))}
+                    </div>
+                </Row>
+            </Container>
         </>
     );
 };
