6423c312a62dabd38e1ae58ae49b92cd36fdde8e
kbeksss <kbeksss@outlook.com>
2020-03-21 16:25:15 +0600

    Created basic files and configurations

--- front/src/index.js
+++ front/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from "connected-react-router";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+import store, {history} from './store/configureStore';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+const app = (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(app, document.getElementById('root'));
 serviceWorker.unregister();
