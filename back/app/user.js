4bb92064cceaa6fb8c9ca32d1c590760b760613d
kbeksss <kbeksss@outlook.com>
2020-03-21 14:10:22 +0600

    made router for users

--- back/app/user.js
+++ back/app/user.js
@@ -1,1 +1,42 @@
 const express = require('express');
+
+const bcrypt = require('bcrypt');
+
+
+const auth = require('../middleware/auth');
+
+const User = require('../models/User');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const user = new User(req.body);
+
+    try{
+        user.generateToken();
+        await user.save();
+        return res.send(user);
+    } catch (e) {
+        return res.status(400).send(e);
+    }
+});
+
+router.post('/sessions', async (req, res) => {
+    const user = await User.findOne({username: req.body.username});
+
+    if(!user){
+        return res.status(400).send({error: 'Username or password not correct!'})
+    }
+
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isMatch) {
+        return res.status(400).send({error: 'Username or password not correct!'})
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return res.send(user);
+});
+
+module.exports = router;
