8e0808c8babda6fde7af243979c6d22dbaf8b5b7
kbeksss <kbeksss@outlook.com>
2020-03-21 11:58:31 +0600

    Created file with database configs (mongoose)

--- back/index.js
+++ back/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const config = require('./config');
+
 const app = express();
 const port = 8000;
 
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const run = async () => {
+    await mongoose.connect(config.database, config.databaseOptions);
     app.listen(port, () => {
         console.log('Server is running on port: ', port);
     })
